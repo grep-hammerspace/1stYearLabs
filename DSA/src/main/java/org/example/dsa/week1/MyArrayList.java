eaab9c01f22ad84a2f4fcc979f99d4efe773f6de
Asad <asadyezdan@gmail.com>
2026-04-24 16:02:09 +0100

    D

--- DSA/src/main/java/org/example/dsa/week1/MyArrayList.java
+++ DSA/src/main/java/org/example/dsa/week1/MyArrayList.java
@@ -4,7 +4,7 @@ public class MyArrayList<T> {
     // Hint: Java does not allow creating a generic array (new T[capacity]) due to type
     // erasure, so we store Object[] and cast on retrieval instead.
     private Object[] data;
-    private final double growthFactor = 2;
+    private final int growthFactor = 2;
     private final int defaultSize = 5;
 
     public MyArrayList() {
